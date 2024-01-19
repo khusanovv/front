559279f212b01d6496f3c6c80f59ca75ba821d23
Boburshoh Khusanov <110517357+khusanovv@users.noreply.github.com>
2024-01-19 23:27:39 +0500

    Update Profil page

--- src/components/profil/profil.jsx
+++ src/components/profil/profil.jsx
@@ -6,7 +6,13 @@ const Profil = () => {
   return (
     <Container>
         <Navbar />
-        <div>Profil</div>
+        <Container>
+          <div className="profil">
+            <div className="profil-main"></div>
+            <div className="profil-img"></div>
+
+          </div>
+        </Container>
     </Container>
   )
 }
