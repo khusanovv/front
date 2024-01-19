dcf6cf6a9f48fd713c30054288be8544ecdd854c
Boburshoh Khusanov <110517357+khusanovv@users.noreply.github.com>
2024-01-19 23:40:35 +0500

    Update Profil page

--- src/components/profil/profil.jsx
+++ src/components/profil/profil.jsx
@@ -5,14 +5,26 @@ import Navbar from '../navbar/Navbar'
 const Profil = () => {
   return (
     <Container>
-        <Navbar />
-        <Container>
-          <div className="profil">
-            <div className="profil-main"></div>
-            <div className="profil-img"></div>
-
+      <Navbar />
+      <Container>
+        <div className="profil">
+          <div className="profil-main">
+            <div className="profil-category"></div>
+            <h2 className="profil-title">Ернар Ибрагимов</h2>
+            <p className="profil info">Работаю дизайнером с 1999 года.
+              Был опыт в газетах, журналах, типографиях, рекламных агентствах, издательских домах... Порядка 8 лет, занимаюсь логотипами и фирменными стилями.</p>
+            <div className="profil-skills"></div>
+            Figma
+            Adobe photoshop
+            Adobe illustrator
+            Corel draw
+            Adobe After Effects
+            HTML/CSS
           </div>
-        </Container>
+          <div className="profil-img"></div>
+
+        </div>
+      </Container>
     </Container>
   )
 }
